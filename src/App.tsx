b52f92f8c8aeb411a87b0cd0d4b33045698c2fb8
idkwhat444 <woorimil6822@gmail.com>
2025-06-10 10:56:13 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
 export default function App() {
   return <h1>RivenTeam 리디자인 시작</h1>;
+import Gallery from './components/Gallery'
+
+export default function App() {
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <Hero />
+      <Pricing />
+      <Gallery />  {/* 여기 추가 */}
+    </div>
+  );
 }
